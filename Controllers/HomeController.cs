b1e6a6eaf29efd5f6a0c43852fb9f35899ffabf4
EevaCareeria23 <132044749+EevaCareeria23@users.noreply.github.com>
2023-11-21 19:22:21 +0200

    Login toimii

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TukiVerkko1.Models;
 
 namespace TukiVerkko1.Controllers
 {
@@ -25,6 +26,37 @@ namespace TukiVerkko1.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Authorize(Logins LoginModel)
+        {
+            TikettiDBEntities db = new TikettiDBEntities();
+
+            var LoggedUser = db.Logins.SingleOrDefault(x => x.Käyttäjätunnus == LoginModel.Käyttäjätunnus && x.Salasana == LoginModel.Salasana);
+            if (LoggedUser != null)
+            {
+                //ViewBag.LoggedStatus = "Paikalla";
+                ViewBag.LoginError = 0;
+                Session["UserName"] = LoggedUser.Käyttäjätunnus;
+                return RedirectToAction("About", "Home");
+            }
+            else
+            {
+                //ViewBag.LoggedStatus = "Vieras";
+                ViewBag.LoginError = 1;
+                LoginModel.KirjautumisVirheIlmoitus = "Tuntematon käyttäjätunnus tai salasana";
+                return View("Index", LoginModel);
+            }
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            ViewBag.LoggedStatus = "Vieras";
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- Models/Logins.cs
+++ Models/Logins.cs
@@ -11,12 +11,17 @@ namespace TukiVerkko1.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Logins
     {
         public int LoginID { get; set; }
+        [Required(ErrorMessage = "Anna käyttäjätunnus!")]
         public string Käyttäjätunnus { get; set; }
+        [Required(ErrorMessage = "Anna salasana!")]
+        [DataType(DataType.Password)]
         public string Salasana { get; set; }
         public string Rooli { get; set; }
+        public string KirjautumisVirheIlmoitus { get; set; }
     }
 }
