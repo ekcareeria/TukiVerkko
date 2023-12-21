0cf1943255952d76dbe50f39402a13cc71d1f471
rehunatunti <106740573+rehunatunti@users.noreply.github.com>
2023-12-21 10:09:27 +0200

    Feature/lomakemerge (#10)
    * Lisätty AsiakkaatsController
    
    * Lisätty ViewModel ja näkymät
    
    * Lisätty feature/lomake tiedostot
    
    * Tukipyyntömetodia päivitetty
    
    * Korjattu linkki Tukipyyntölomakkeessa
    
    ---------
    
    Co-authored-by: EevaCareeria23 <132044749+EevaCareeria23@users.noreply.github.com>

---
+++ Controllers/AsiakkaatsController.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TukiVerkko1.Models;
+using TukiVerkko1.ViewModels;
+  
+
+namespace TukiVerkko1.Controllers
+{
+    public class AsiakkaatsController : Controller
+    {
+        private TikettiDBEntities1 db = new TikettiDBEntities1();
+
+        // GET: Asiakkaats
+        public ActionResult Index()
+        {
+            //var asiakkaat = db.Asiakkaat;
+            return RedirectToAction("Index", "HomeController", new {});
+
+        }
+
+        // GET: Asiakkaats/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Asiakkaat asiakkaat = db.Asiakkaat.Find(id);
+            if (asiakkaat == null)
+            {
+                return HttpNotFound();
+            }
+            return View(asiakkaat);
+        }
+
+        // GET: Asiakkaats/Create
+        public ActionResult Create()
+        {
+            // Dropdown
+            ViewBag.TikettiID = new SelectList(db.Tiketit, "TikettiID", "Otsikko");
+            return View();
+        }
+
+        // POST: Asiakkaats/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AsiakasID,TikettiID,Etunimi,Sukunimi,Puhelinnumero,Sähköposti")] Asiakkaat asiakkaat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Asiakkaat.Add(asiakkaat);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            //// Tämä SelectList liittynee vain dropdowniin
+            //ViewBag.TikettiID = new SelectList(db.Tiketit, "TikettiID", "Otsikko", asiakkaat.TikettiID);
+            return View(asiakkaat);
+        }
+
+        // GET: Asiakkaats/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Asiakkaat asiakkaat = db.Asiakkaat.Find(id);
+            if (asiakkaat == null)
+            {
+                return HttpNotFound();
+            }
+            //ViewBag.TikettiID = new SelectList(db.Tiketit, "TikettiID", "Otsikko", asiakkaat.TikettiID);
+            return View(asiakkaat);
+        }
+
+        // POST: Asiakkaats/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AsiakasID,TikettiID,Etunimi,Sukunimi,Puhelinnumero,Sähköposti")] Asiakkaat asiakkaat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(asiakkaat).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ////Tämä oletuksena luotu, meni rikki kun malli päivitettiin
+            //ViewBag.TikettiID = new SelectList(db.Tiketit, "TikettiID", "Otsikko", asiakkaat.TikettiID);
+            return View(asiakkaat);
+        }
+
+        // GET: Asiakkaats/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Asiakkaat asiakkaat = db.Asiakkaat.Find(id);
+            if (asiakkaat == null)
+            {
+                return HttpNotFound();
+            }
+            return View(asiakkaat);
+        }
+
+        // POST: Asiakkaats/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Asiakkaat asiakkaat = db.Asiakkaat.Find(id);
+            db.Asiakkaat.Remove(asiakkaat);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: Create-NÄKYMÄ tukipyyntölomakkeelle:
+        public ActionResult Tukipyynto() 
+        {
+            ViewBag.KategoriaID = new SelectList(db.Kategoriat, "KategoriaID", "Nimi");
+            return View();
+        }
+        // POST: Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Tukipyynto([Bind(Include = "AsiakasID,Otsikko,KategoriaID,Etunimi,Sukunimi,Sähköposti,Puhelinnumero,Kuvaus")] Lomaketiedot tiedot)
+        {
+            if (ModelState.IsValid)
+            {
+                var tiketti = new Tiketit()
+                {
+                    Otsikko = tiedot.Otsikko,
+                    Kuvaus = tiedot.Kuvaus,
+                    KategoriaID = tiedot.KategoriaID,
+                    AsiakasID = tiedot.AsiakasID,
+                    Status = "Avoin",  
+                };
+
+                DateTime aikanyt = DateTime.Now;
+                tiketti.Aika = aikanyt;
+                db.Tiketit.Add(tiketti);
+
+                var asiakas = new Asiakkaat()
+                {
+                    Etunimi = tiedot.Etunimi,
+                    Sukunimi = tiedot.Sukunimi,
+                    Sähköposti = tiedot.Sähköposti,
+                    Puhelinnumero = tiedot.Puhelinnumero
+                };
+
+                db.Asiakkaat.Add(asiakas);
+                db.SaveChanges();
+                
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.KategoriaID = new SelectList(db.Kategoriat, "KategoriaID", "Nimi", tiedot.KategoriaID);
+
+            return View(tiedot);
+        }
+        
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        
+    }
+}
---
+++ ViewModels/Lomaketiedot.cs
@@ -0,0 +1,30 @@
+﻿
+//using TukiVerkko1.Models;
+
+namespace TukiVerkko1.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Web.Mvc;
+
+    public class Lomaketiedot
+    {
+
+        public int AsiakasID { get; set; }      //Copypastattu Elenalta
+        public int TikettiID { get; set; }
+        public string Etunimi { get; set; }
+        public string Sukunimi { get; set; }
+        public string Puhelinnumero { get; set; }
+        public string Sähköposti { get; set; }
+        public int KategoriaID { get; set; }
+        public string Otsikko { get; set; }
+        public string Kuvaus { get; set; }
+        public Nullable<System.DateTime> Aika { get; set; }
+        public Nullable<System.DateTime> Valmistumisaika { get; set; }
+        public string Nimi { get; set; }
+        //public string Status { get; set; }
+
+    }
+    
+}
