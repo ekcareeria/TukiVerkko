24c204a61b1413325e01b1acb83679f98eada6d3
Tiia Virtanen <tiia.virtanen@student.careeria.fi>
2024-01-03 18:27:43 +0200

    Päivityksiä HomeControlleriin roolien osalta

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -37,6 +37,7 @@ namespace TukiVerkko1.Controllers
             {
                 ViewBag.LoginVirhe = 0;
                 Session["Käyttäjätunnus"] = Kirjautunut.Käyttäjätunnus;
+                Session["Rooli"] = HaeRooliTietokannasta(Kirjautunut.Käyttäjätunnus);
                 return RedirectToAction("TikettiOtsikot", "Tiketit");
             }
             else
@@ -57,20 +58,35 @@ namespace TukiVerkko1.Controllers
             return RedirectToAction("Index", "Home");
         }
 
-        //[Authorize] // Varmista, että käyttäjä on kirjautunut sisään
-        //[HttpGet]
-        //public ActionResult HaeRooli()
-        //{
-        //    TikettiDBEntities1 db = new TikettiDBEntities1();
-        //    Tarkistetaan käyttäjän rooli
-        //    string käyttäjänRooli = HttpContext.Current.Session["Rooli"] as string;
+        [Authorize] // Varmista, että käyttäjä on kirjautunut sisään
+        [HttpGet]
+        public ActionResult HaeRooli()
+        {
+            // Tarkistetaan käyttäjän rooli Sessionista
+            string käyttäjänRooli = Session["Rooli"] as string;
 
-        //    if (käyttäjänRooli != null && käyttäjänRooli.Equals("Ylläpitäjä", StringComparison.OrdinalIgnoreCase))
-        //    {
-        //        return true; // Palauta true, jos käyttäjällä on oikea rooli
-        //    }
+            if (käyttäjänRooli != null)
+            {
+                return Content(käyttäjänRooli);
+            }
+            else
+            {
+                return Content("Vieras");
+            }
+        }
 
-        //    return Json(rooli);
-        //}
+        private string HaeRooliTietokannasta(string käyttäjätunnus)
+        {
+            TikettiDBEntities1 db = new TikettiDBEntities1();
+
+            var käyttäjä = db.Logins.SingleOrDefault(u => u.Käyttäjätunnus == käyttäjätunnus);
+
+            if (käyttäjä != null)
+            {
+                return käyttäjä.Rooli;
+            }
+
+            return "Vieras";
+        }
     }
 }
